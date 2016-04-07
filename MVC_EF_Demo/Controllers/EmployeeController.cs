f9849588fdbdfea4c385abb8731238c8f59dc16a
skakkunuri <srikanth.kakkunuri@gmail.com>
2016-04-07 23:37:03 +0530

    Added project from home

---
+++ MVC_EF_Demo/Controllers/EmployeeController.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using EmployeeBL.EF;
+using EmployeeBL;
+using MVC_EF_Demo.Models;
+namespace MVC_EF_Demo.Controllers
+{
+    public class EmployeeController : Controller
+    {
+        // GET: Employee
+        public ActionResult GetAllEplmoyees()
+        {
+            Viewmodel vm = new Viewmodel();
+            vm.Employees = GetAllEmployees();
+            vm.Departments = GetAllDepartments();  
+            return View(vm);
+        }
+
+
+        #region "Private functions"
+
+        private List<Employee> GetAllEmployees()
+        {
+            MyFirstDBEntities myDB = new MyFirstDBEntities();
+            return (myDB.Employees.ToList());
+        }
+        private List<Department> GetAllDepartments()
+        {
+            MyFirstDBEntities myDB = new MyFirstDBEntities();
+            return (myDB.Departments.ToList());
+        }
+
+
+        # endregion
+
+    }
+}
---
+++ MVC_EF_Demo/Models/Viewmodel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using EmployeeBL;
+using EmployeeBL.EF;
+
+namespace MVC_EF_Demo.Models
+{
+    public class Viewmodel
+    {
+        public IEnumerable<Employee> Employees { get; set; }
+        public IEnumerable<Department> Departments { get; set; }
+
+    }
+}
