2efad36daf2be5d9762a1a583a5bdb687f6a5742
skakkunuri <srikanth.kakkunuri@gmail.com>
2016-04-09 00:10:47 +0530

    Added new project from home

---
+++ JQuery_AutoFill_Demo/Controllers/PersonController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using JQuery_AutoFill_Demo.Models;
+
+namespace JQuery_AutoFill_Demo.Controllers
+{
+    public class PersonController : Controller
+    {
+        // GET: Person
+        public ActionResult Index()
+        {
+            using (AdventureWorks2014Entities1 db = new AdventureWorks2014Entities1())
+            {
+                var X = from r in db.People
+                        select new { r.FirstName, r.LastName };
+
+                //var v = db.People.ToList();
+                return View(X.ToList());
+            }
+                
+        }
+    }
+}
